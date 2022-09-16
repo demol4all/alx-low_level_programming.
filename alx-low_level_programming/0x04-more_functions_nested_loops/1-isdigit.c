e440c5dfae1b9957847f68c7c531784256674d8f
demol4all <111072536+demol4all@users.noreply.github.com>
2022-09-15 19:39:10 -0700

    Update 1-isdigit.c

--- alx-low_level_programming/0x04-more_functions_nested_loops/1-isdigit.c
+++ alx-low_level_programming/0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,1 +1,13 @@
-hguyj
+/**
+ * _isdigit - determine if digit
+ *
+ * @c: character to determine
+ *
+ * Return: gives boolean
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
